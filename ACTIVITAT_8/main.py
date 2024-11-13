49311e2c1a5b6d54eb4b5a566a833f35d3353f36
Brandy <2023_brandy.sherile@iticbcn.cat>
2024-11-13 20:29:59 +0100

    Post method created

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,14 +1,25 @@
-## rsobrinog
 from fastapi import FastAPI
 from pydantic import BaseModel
-
-from fastapi import FastAPI
+from typing import Union
 
 app = FastAPI()
 
+# Employee model for normal responses
+class Employee(BaseModel):
+    employee_id: int
+    name: str
+    department: str
+    position: str
+    start_date: str
+    salary: float | None = None
 
 @app.get("/")
 async def root():
     return {"Activitat FastApi"}
+
+@app.post("/employee/")
+async def create_item(employee: Employee):
+    return {"employee": employee}
+
 
 
