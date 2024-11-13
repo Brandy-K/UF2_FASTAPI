4baaf1b30ff0b51cd27554b542d6aaba781c8354
Brandy <2023_brandy.sherile@iticbcn.cat>
2024-11-13 20:50:16 +0100

    response 404 created

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from typing import Union
+
 
 app = FastAPI()
+
 
 # Employee model for normal responses
 class Employee(BaseModel):
@@ -13,13 +15,36 @@ class Employee(BaseModel):
     start_date: str
     salary: float | None = None
 
+
+data_employee = {
+    1: {
+        "employee_id": 1,
+        "name": "Brandy",
+        "department": "IT",
+        "position": "Intern",
+        "start_date": "2023-01-01",
+        "salary": 0
+    }
+}
+
+
 @app.get("/")
 async def root():
     return {"Activitat FastApi"}
+
 
 @app.post("/employee/")
 async def create_item(employee: Employee):
     return {"employee": employee}
 
+
+@app.get("/employee/{employee_id}")
+async def get_employee(employee_id: int):
+    employee = data_employee.get(employee_id)
+
+    if not employee:
+        return JSONResponse(status_code=404, content={"detail": "Employee not found"})
+
+    return {"employee": employee}
 
 
