3234e69b8eb832b06f290020cd7a918124005625
Brandy <2023_brandy.sherile@iticbcn.cat>
2024-11-24 18:34:54 +0100

    crud and main updated

--- ACTIVITAT_9/crud.py
+++ ACTIVITAT_9/crud.py
@@ -5,7 +5,8 @@ def insert_users():
     conn = get_connection()
     cursor = conn.cursor()
     try:
-        cursor.execute("INSERT INTO Users VALUES %s, %s, %s")
+        cursor.execute("INSERT INTO users (name, surname) VALUES (%s, %s)", ("Brandy", "Kisia"))
+        cursor.execute("INSERT INTO users (name, surname) VALUES (%s, %s)", ("Doua", "Alabed"))
         conn.commit()
     finally:
         conn.close()
@@ -15,28 +16,28 @@ def read_users():
     conn = get_connection()
     cursor = conn.cursor()
     try:
-        cursor.execute("SELECT id, name, surname FROM Users;")
+        cursor.execute("SELECT id, name, surname FROM users;")
         users = cursor.fetchall()
         return users
     finally:
         conn.close()
 
 
-def update_users(user_id, new_name, new_surname):
+def update_users(Id, name, surname):
     conn = get_connection()
     cursor = conn.cursor()
     try:
-        cursor.execute("UPDATE Users SET name = %s, surname = %s WHERE id = %s")
+        cursor.execute("UPDATE users SET name = %s, surname = %s WHERE id = %s", (name, surname, Id))
         conn.commit()
     finally:
         conn.close()
 
 
-def delete_users(user_id):
+def delete_users(Id):
     conn = get_connection()
     cursor = conn.cursor()
     try:
-        cursor.execute("DELETE FROM Users WHERE id = %s", (user_id,))
+        cursor.execute("DELETE FROM Users WHERE id = %s", (Id,))
         conn.commit()
     finally:
         conn.close()
--- ACTIVITAT_9/main.py
+++ ACTIVITAT_9/main.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from crud import read_users, insert_users, delete_users
+import schemas
 
-import users
 app = FastAPI()
 
+# Para crear Usuaris
 
-class User(BaseModel):
-    id: int
-    name: str
-    surname: str
+
+@app.post("/users")
+async def create_users():
+    insert_users()  # Calls the insert function to add users to the database
+    return {"message": "Users added successfully"}
+
+# Para llegir Usuaris
 
 
 @app.get("/users", response_model=list[dict])
-async def read_users():
-    return users.users_schema(users.read())
+async def read_users_endpoint():
+    users = read_users()  # Retorna users des de database
+    return schemas.users_schema(users)
+
+# Para Eliminar Usuaris
+
+
+@app.delete("/users/{user_id}")
+async def delete_user(user_id: int):
+    delete_users(user_id)  # Deletes the user by id
+    return {"message": f"User with id {user_id} has been deleted"}
+
